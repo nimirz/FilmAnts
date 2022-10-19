b7d16730fb8d4000bfc6018d3aa5569583f29f8a
Fabian Plum <fabianplum@web.de>
2022-10-19 11:15:18 +0100

    Added further resolution, whitebalance, frame rate and auto-decoding options via ffmpeg

--- encode.py
+++ encode.py
@@ -4,6 +4,8 @@
 import depthai as dai
 import cv2
 import argparse
+import subprocess
+from datetime import datetime
 
 # Construct argument parser and parse arguments
 ap = argparse.ArgumentParser()
@@ -11,6 +13,12 @@ ap.add_argument("-o", "--output", required=True, help="path to output video file
 ap.add_argument("-e", "--exposure", required=False, help="set exposure of camera")
 ap.add_argument("-l", "--lens", required=False, help="set lens position of camera")
 ap.add_argument("-i", "--iso", required=False, help="set ISO of camera")
+ap.add_argument("-t", "--temp", required=False, help="set colour temperature for manual white balance")
+ap.add_argument("-4k", "--record_4K", type=bool, required=False, default=False, help="record in 4K resolution")
+ap.add_argument("-fps", "--frame_rate", type=float, required=False, default=30, help="set desired framerate")
+ap.add_argument("-auto", "--auto_decode", type=bool, required=False, default=False,
+                help="automatically produce mp4 file (requires ffmpeg to be installed)")
+
 args = vars(ap.parse_args())
 
 # Update camera settings
@@ -29,10 +37,16 @@ if args["iso"]:
 else:
     sensIso = 1200
 
+if args["temp"]:
+    colourTemp = int(args["temp"])
+else:
+    colourTemp = 6000
+
 # Step size for manual exposure/focus
 EXP_STEP = 500
 ISO_STEP = 50
 LENS_STEP = 3
+COL_STEP = 100
 
 # Defaults and limits for manual focus/exposure controls
 lensMin = 0
@@ -44,19 +58,25 @@ expMax = 33000
 sensMin = 100
 sensMax = 1600
 
+col_min = 1000
+col_max = 12000
+
 # Start defining a pipeline
 pipeline = dai.Pipeline()
 
 # Define a source - color camera
 cam = pipeline.create(dai.node.ColorCamera)
 # cam.setBoardSocket(dai.CameraBoardSocket.RGB)
-# cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
-cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+if args["record_4K"]:
+    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+else:
+    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+
 cam.setPreviewSize(1280, 720)
 
 # Create an encoder, consuming the frames and encoding them using H.265 encoding
 videoEncoder = pipeline.createVideoEncoder()
-videoEncoder.setDefaultProfilePreset(30, dai.VideoEncoderProperties.Profile.H265_MAIN)
+videoEncoder.setDefaultProfilePreset(args["frame_rate"], dai.VideoEncoderProperties.Profile.H265_MAIN)
 cam.video.link(videoEncoder.input)
 
 # Create output
@@ -79,18 +99,21 @@ def clamp(num, v0, v1):
     return max(v0, min(num, v1))
 
 
+now = datetime.now()
+
 # Pipeline defined, now the device is connected to
-with dai.Device(pipeline) as device, open(args["output"], 'wb') as videoFile:
+with dai.Device(pipeline) as device, open(args["output"][:-5] + now.strftime("%Y-%m-%d_%H-%M-%S-%MS.h265"),
+                                          'wb') as videoFile:
     controlQueue = device.getInputQueue('control')
     ctrl = dai.CameraControl()
     ctrl.setManualExposure(expTime, sensIso)
     ctrl.setManualFocus(lensPos)
+    ctrl.setManualWhiteBalance(colourTemp)
     controlQueue.send(ctrl)
 
     # Output queue will be used to get the encoded data from the output defined above
     q_rgb = device.getOutputQueue("preview")
     q_enc = device.getOutputQueue(name="h265", maxSize=30, blocking=True)
-
 
     while True:
         in_rgb = q_rgb.tryGet()
@@ -140,6 +163,24 @@ with dai.Device(pipeline) as device, open(args["output"], 'wb') as videoFile:
             ctrl = dai.CameraControl()
             ctrl.setManualExposure(expTime, sensIso)
             controlQueue.send(ctrl)
+        elif key in [ord('h'), ord('j')]:
+            if key == ord('h'): colourTemp -= COL_STEP
+            if key == ord('j'): colourTemp += COL_STEP
+            colourTemp = clamp(colourTemp, col_min, col_max)
+            print("Setting manual whitebalance, kelvin:", colourTemp)
+            ctrl = dai.CameraControl()
+            ctrl.setManualWhiteBalance(colourTemp)
+            controlQueue.send(ctrl)
 
-    print("To view the encoded data, convert the stream file (.h265) into a video file (.mp4) using a command below:")
-    print("ffmpeg -framerate 30 -i video.h265 -c copy video.mp4")
+    decode_command = "ffmpeg -framerate " + str(args["frame_rate"]) + " -i " + \
+                     args["output"][:-5] + now.strftime("%Y-%m-%d_%H-%M-%S-%MS.h265") + " -c copy " + \
+                     args["output"][:-5] + now.strftime("%Y-%m-%d_%H-%M-%S-%MS.mp4")
+
+    if args["auto_decode"]:
+        ret = subprocess.run(decode_command, capture_output=True)
+        print("Processing output file...")
+        print(ret, "\n\n Finished conversion!")
+    else:
+        print(
+            "To view the encoded data, convert the stream file (.h265) into a video file (.mp4) using a command below:")
+        print(decode_command)
